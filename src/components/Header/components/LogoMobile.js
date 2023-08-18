0932066c6b159d56562d9261c4645408f7a844a4
Chen <jonkiky@gmail.com>
2023-08-18 16:23:27 -0400

    update 404 page

--- src/components/Header/components/LogoMobile.js
+++ src/components/Header/components/LogoMobile.js
@@ -4,7 +4,10 @@ import { headerData } from '../../../config/globalHeaderData';
 
 const LogoArea = styled.div`
     display: flex;
-
+    img {
+      width: fit-content;
+      height: 56px;
+    }
     .logoContainer {
       margin-top: 32px;
     }
