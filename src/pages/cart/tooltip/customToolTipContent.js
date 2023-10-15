051bc13c00b5e8d0fb425280006acf432cad2fb6
Senait7-ux <119904111+Senait7-ux@users.noreply.github.com>
2023-10-14 23:57:39 -0400

    Finished task

---
+++ src/pages/cart/tooltip/customToolTipContent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { withStyles } from "@material-ui/core";
+import Styles from './customToolTipContent.style';
+
+const CustomToolTipContent = ({ classes }) => (
+    <div className={classes.downloadFileManifestTooltip}>
+        {/* eslint-disable-next-line max-len */}
+        To access and analyze files, select and remove unwanted files, click the "Download File Manifest" button, and upload the resulting manifest file to your         {' '}
+        <a
+            style={{ color: '#blue' }}
+            target="_blank"
+            rel="noreferrer"
+            href="https://cgc-accounts.sbgenomics.com/auth/login?next=https%3A%2F%2Fcgc-accounts.sbgenomics.com%2F"
+        >
+            <span style={{ textDecoration: 'underline', margin: 0, padding: 0 }}>
+                Velsera Seven Bridges Cancer Genomics Cloud account
+            </span>
+        </a>
+        {' '}
+        account.
+    </div>
+)
+
+export default withStyles(Styles, { withTheme: true })(CustomToolTipContent);
