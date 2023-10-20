04d3ea0bb75b79a99ae8ef4920560d412d9eef14
adamdavenportGov <adam.davenport@nih.gov>
2023-10-20 15:20:08 -0400

    Changed names to match the backend

--- src/pages/dashTemplate/DashTemplateController.js
+++ src/pages/dashTemplate/DashTemplateController.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { CircularProgress } from '@material-ui/core';
 import { getFilters } from '@bento-core/facet-filter';
 import DashTemplateView from './DashTemplateView';
-import { DASHBOARD_QUERY_NEW } from '../../bentoc/dashboardTabData';
+import { DASHBOARD_QUERY_NEW } from '../../bento/dashboardTabData';
 
 const getDashData = (states) => {
   const {
@@ -17,7 +17,7 @@ const getDashData = (states) => {
     const result = await client.query({
       query: DASHBOARD_QUERY_NEW,
       variables: activeFilters,
-      // context: { clientName: "mockService"},
+      context: { clientName: "localService"},
     })
       .then((response) => response.data);
     return result;
@@ -36,8 +36,8 @@ const getDashData = (states) => {
   useEffect(() => {
     const controller = new AbortController();
     getData(activeFilters).then((result) => {
-      if (result.searchSubjects) {
-        setDashData(result.searchSubjects);
+      if (result.searchParticipants) {
+        setDashData(result.searchParticipants);
       }
     });
     return () => controller.abort();
