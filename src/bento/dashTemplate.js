022742d0cda9a04a863b3759b21f6935d8e88b34
Chen <jonkiky@gmail.com>
2024-03-06 14:37:20 -0500

    Change 'exposures' front end label in facets to 'environmental exposures'

--- src/bento/dashTemplate.js
+++ src/bento/dashTemplate.js
@@ -111,7 +111,7 @@ export const facetsConfig = [
   },
   {
     section: CASES,
-    label: 'Exposures',
+    label: 'Environmental Exposures',
     apiPath: 'participantCountByCarcinogenExposure',
     apiForFiltering: 'filterParticipantCountByCarcinogenExposure',
     datafield: 'carcinogen_exposure',
