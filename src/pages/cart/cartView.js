08d8529462c1c2a7d85f18351279edaff3f0c9cd
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2024-03-14 18:04:34 -0400

    remove unused useEffect

--- src/pages/cart/cartView.js
+++ src/pages/cart/cartView.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Grid, withStyles } from '@material-ui/core';
 import { TableContext, TableView } from '@bento-core/paginated-table';
 import { configColumn } from './tableConfig/Column';
