bd410da8e39d186e5b56b256b402f4a1a9a79a72
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2024-03-26 11:08:41 -0400

    Refactor stats bar to be more responsive

--- src/bento/globalStatsData.js
+++ src/bento/globalStatsData.js
@@ -7,26 +7,90 @@ export const statsStyling = {
     height: '50px',
     background: '#47AEC3',
   },
-  statsGroup: {
-    margin: '6px 9.25px',
-    padding: '0.1% 10% 2% 6%',
-    borderRight: 'none',
-    '&:first-child': {
-      padding: '0.1% 6% 2% 6%',
+  box: {
+    '@media (max-width: 860px)': {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+
+      '& > *': {
+        flexBasis: '33.33%',
+        height: '47px'
+      },
+      '& > *:nth-child(3), & > *:last-child': {
+        width: '140px',
+        maxWidth: '140px',
+        height: '47px'
+      },
     },
+    '@media (max-width: 610px)': {
+      marginLeft: '16px',
+      '& > *': {
+        flexBasis: '50%',
+        height: '47px'
+      },
+      '& > *:nth-child(3), & > *:last-child': {
+        width: '50%',
+        maxWidth: '50%',
+      },
+      '& > *:nth-child(1), & > *:nth-child(3), & > *:nth-child(5)': {
+        width: 'calc(50% - 50px)',
+        maxWidth: 'calc(50% - 50px)',
+      },
+    },
+    '@media (max-width: 425px)': {
+      alignItem: 'center',
+      paddingLeft: '16px',
+      margin: 'auto',
+      '& > *': {
+        flexBasis: '100%',
+        height: '47px'
+      },
+      '& > *:nth-child(3), & > *:last-child': {
+        width: '100%',
+        maxWidth: '100%',
+      },
+      '& > *:nth-child(1), & > *:nth-child(3), & > *:nth-child(5)': {
+        width: '100%',
+        maxWidth: '100%',
+      },
+    }
+  },
+  statsGroup: {
+    height: '47px',
+    borderRight: 'none',
+    margin: '0px',
+    padding: '4px 80px 10px 40px',
+
     '&:nth-child(2)': {
       '& $statsIcon': { // Targeting statsIcon inside the second child
         marginTop: '4px',
       },
     },
-    '&:last-child': {
-      padding: '0.1% 6% 2% 6%',
+    '&:nth-child(5)': {
+      '& $statsIcon': { // Targeting statsIcon inside the fifth child
+        marginTop: '2px',
+      },
     },
+    '&:last-child': {
+      padding: '4px 0px 10px 40px'
+    },
+
+    '@media (max-width: 1200px)': {
+      padding: '4px 15px 10px 40px',
+    },
+    '@media (max-width: 880px)': {
+      padding: '4px 15px 10px 40px',
+    },
+    '@media (max-width: 425px)': {
+      margin: 'auto 30%',
+      width: 'fit-content',
+    }
   },
   statsIcon: {
     width: '40px',
     height: '45px',
-    margin: '0px 0px 0px -45px',
+    margin: '0px 0px 0px -40px',
     position: 'relative',
   },
   statCount: {
@@ -34,7 +98,7 @@ export const statsStyling = {
     fontWeight: '500',
     fontFamily: 'Oswald',
     fontSize: '20px',
-    margin: '-5px 0px -4px 10px',
+    margin: '-5px 0px -4px 8px',
     paddingTop: '0px',
     // lineHeight: 17px
   },
@@ -44,7 +108,9 @@ export const statsStyling = {
     fontWeight: '700',
     fontSize: '12px',
     textTransform: 'none',
-    margin: '0px 0px 0px 10px',
+    margin: '0px 0px 0px 8px',
+    width: 'fit-content',
+    whiteSpace: 'nowrap',
   },
 };
 
