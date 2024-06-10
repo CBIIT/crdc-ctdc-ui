5ce26e4a014f487fb4b8265b2eedfa081adbdd1d
adamdavenportGov <adam.davenport@nih.gov>
2024-06-10 13:33:06 -0400

    Adjustment for CTDC-1506
    I added a first title so the title spacing isn't effected at the top of the page,

--- src/components/About/aboutBodyView.js
+++ src/components/About/aboutBodyView.js
@@ -3,6 +3,7 @@ import { Grid, Link, withStyles } from '@material-ui/core';
 import XoomInOut from './xoomInOutView';
 import tableExternalIcon from './assets/About-Table-ExternalLink.svg';
 import externalLinkIcon from './assets/About-ExternalIcon.svg';
+import { title } from '../../bento/studyDetailData';
 
 const AboutBody = ({ classes, data, externalIconImage }) => {
   function boldText(text) {
@@ -121,6 +122,10 @@ const AboutBody = ({ classes, data, externalIconImage }) => {
                         // For sub headings
                         if (splitedParagraph != null && (/#(.*)#/.test(splitedParagraph))) {
                           return (<div className={classes.title}>{splitedParagraph.match(/#(.*)#/).pop()}</div>);
+                        }
+                        // for first sub heading on page
+                        if (splitedParagraph != null && (/~(.*)~/.test(splitedParagraph))) {
+                          return (<div className={classes.firstTitle}>{splitedParagraph.match(/~(.*)~/).pop()}</div>);
                         }
                         // For Italize inline words
                         if (splitedParagraph != null && (/!(.*)!/.test(splitedParagraph))) {
@@ -306,7 +311,7 @@ const styles = () => ({
   indentedText: {
     // height: '476px',
     // width: '675px',
-    margin: '10px auto 0px 30px',
+    margin: '0px auto 0px 30px',
     color: '#000000',
     fontFamily: (props) => (props.data.fontFamily ? props.data.fontFamily : 'Nunito'),
     fontSize: '16px',
@@ -317,9 +322,22 @@ const styles = () => ({
     fontWeight: '400',
     fontSize: '22px',
     fontFamily: 'Inter',
-    margin: '0px 0px 8px 0px'
-
+    margin: '42px 0px 0px 0px',
+    '&:first-of-type': {
+      margin: '0px 0px 0px 0px',
+    },
+    '&:first-child': {
+      margin: '42px 0px 0px 0px',
+    },
   }),
+  firstTitle: (props) => ({
+    color: props.titleColor,
+    fontWeight: '400',
+    fontSize: '22px',
+    fontFamily: 'Inter',
+    margin: '0px 0px 0px 0px'
+  }),
+  
   italicizeText: (props) => ({
     color: props.titleColor,
     fontStyle: 'italic',
