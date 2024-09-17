2dcf911540c0937bd4af50dc5cf80f83eca59a7e
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2024-09-17 12:41:00 -0400

    Refactor static content to be fetched from API instead of local YAML file

--- src/pages/about/aboutController.js
+++ src/pages/about/aboutController.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import yaml from 'js-yaml';
 import axios from 'axios';
-import YAMLData from '../../content/prod/aboutPagesContent.yaml';
 import AboutView from './aboutView';
-import { useQuery } from '@apollo/client';
+import env from '../../utils/env';
 
 const About = ({ match }) => {
   const [data, setData] = useState([]);
 
+const ABOUT_CONTENT_URL = env.REACT_APP_ABOUT_CONTENT_URL;
 
   useEffect(() => {
     const fetchData = async () => {
       let resultData = [];
       let result = [];
       try {
-        result = await axios.get(YAMLData);
+        result = await axios.get(ABOUT_CONTENT_URL);
         resultData = yaml.safeLoad(result.data);
         const supportObj = resultData.find(({ page }) => page === match.path);
         setData(supportObj);
@@ -30,7 +30,6 @@ const About = ({ match }) => {
   }
 
   return (
-
     <AboutView data={data} />
   );
 };
