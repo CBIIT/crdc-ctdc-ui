598723922b8c41b43916183b876a0713ff2040d1
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2025-01-16 16:34:21 -0500

    Update Cart Page

--- src/bento/fileCentricCartWorkflowData_OLD.js
+++ src/bento/fileCentricCartWorkflowData_OLD.js
@@ -98,7 +98,7 @@ export const myFilesPageData = {
 };
  
 export const manifestData = {
-  keysToInclude: ['data_file_name', 'data_file_uuid','data_file_uuid', 'data_file_checksum_value','subject_id', 'parent_specimen_id', 'ctep_disease_term','meddra_disease_code', 'primary_disease_site','histology', 'stage_of_disease','tumor_grade', 'age_at_enrollment', 'sex', 'reported_gender', 'race','ethnicity','carcinogen_exposure','targeted_therapy','parent_specimen_id','anatomical_collection_site','tissue_category','assessment_timepoint','User_Comment'],
+  keysToInclude: ['data_file_name', 'data_file_uuid','data_file_uuid', 'data_file_checksum_value','participant_id', 'parent_specimen_id', 'ctep_disease_term','meddra_disease_code', 'primary_disease_site','histology', 'stage_of_disease','tumor_grade', 'age_at_enrollment', 'sex', 'reported_gender', 'race','ethnicity','carcinogen_exposure','targeted_therapy','parent_specimen_id','anatomical_collection_site','tissue_category','assessment_timepoint','User_Comment'],
   header: ['name', 'drs_uri' ,'File ID', 'Md5sum','Participant ID', 'Biospecimen ID', 'Diagnosis','MedDRA Disease Code', 'Primary Site','Histology', 'Stage of Disease', 'Tumor Grade', 'Age', 'Sex', 'Gender', 'Race', 'Ethnicity', 'Carcinogen Exposure', 'Targeted Therapy', 'Parent Biospecimen ID', 'Anatomical Collection Site','Tissue Category','Collection Timepoint','User Comment'],
 };
 
@@ -126,7 +126,7 @@ export const GET_MY_CART_DATA_QUERY = gql`
       meddra_disease_code
       histology
       data_file_description
-      subject_id
+      participant_id
       primary_disease_site
       specimen_id
       ctep_disease_term
@@ -169,7 +169,7 @@ query fileOverview(
     data_file_size
     association
     data_file_description
-    subject_id
+    participant_id
     primary_disease_site
     parent_specimen_id
     specimen_id
@@ -211,7 +211,7 @@ export const GET_MY_CART_DATA_QUERY_DESC = gql` query filesInList(
     data_file_size
     association
     data_file_description
-    subject_id
+    participant_id
     ctep_disease_term
     meddra_disease_code
     histology
@@ -321,7 +321,7 @@ export const table = {
       role: cellTypes.DISPLAY,
     },
     {
-      dataField: 'subject_id',
+      dataField: 'participant_id',
       header: 'Participant ID',
       display: true,
       tooltipText: 'sort',
