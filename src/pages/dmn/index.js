b8cb506e8a578d97ab180e994b2f2fa75bf6c55d
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2025-04-04 16:28:21 -0400

    Add fall back URL

--- src/pages/dmn/index.js
+++ src/pages/dmn/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import env from '../../utils/env';
 
 const DataModelNavigator = () => {
-  const dmnUrl = env.REACT_APP_DMN_URL;
+  const dmnUrl = env.REACT_APP_DMN_URL || 'https://cbiit.github.io/crdc-data-model-navigator/?config=https://raw.githubusercontent.com/CBIIT/ctdc-data-model-navigator-landing/refs/heads/main/example/CTDC/1.0.0/';
   return (
     <div style={{ width: '100%', height: '1000px' }}>
       <iframe
