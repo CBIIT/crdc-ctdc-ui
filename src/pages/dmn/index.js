c4fb25f278c61c76eedfde70aca3f7cde3fecbcc
Chen <jonkiky@gmail.com>
2025-04-07 15:03:29 -0400

    add allow-popups

--- src/pages/dmn/index.js
+++ src/pages/dmn/index.js
@@ -10,7 +10,7 @@ const DataModelNavigator = () => {
         scrolling="no"
         title="Data Model Navigator"
         style={{ width: '100%', height: '1000px', border: 'none' }}
-        sandbox="allow-scripts allow-same-origin"
+        sandbox="allow-popups allow-scripts allow-same-origin"
       />
     </div>
   );
