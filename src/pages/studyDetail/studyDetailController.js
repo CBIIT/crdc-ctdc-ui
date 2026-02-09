146d628a7ac8acfa2b39505d3831a06b26dd3987
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2026-02-09 10:06:29 -0500

    Add ErrorMessage and refactor controller

--- src/pages/studyDetail/studyDetailController.js
+++ src/pages/studyDetail/studyDetailController.js
@@ -2,23 +2,47 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import StudyView from "./studyDetailView";
-import { Typography } from "../../components/Wrappers/Wrappers";
+import ErrorMessage from "./ErrorMessage";
 import { GET_STUDY_DETAIL_DATA_QUERY } from "../../bento/studyDetailData";
 
+/**
+ * Validates if study data exists in the query response
+ */
+const hasValidStudyData = (data) => {
+  return (
+    data && data.studyByStudyShortName && data.studyByStudyShortName.length > 0
+  );
+};
+
+/**
+ * StudyDetailController
+ * Fetches and displays study detail information
+ *
+ * @param {Object} props - Component props
+ * @param {Object} props.match - React Router match object containing route params
+ */
 const StudyDetailController = ({ match }) => {
+  // TODO: Update API to use study_id from match params instead of hardcoded value
+  const studyId = match.params.id;
+  const studyShortName = "CMB";
+
+
   const { loading, error, data } = useQuery(GET_STUDY_DETAIL_DATA_QUERY, {
-    variables: { study_short_name: ["CMB"] },
+    variables: { study_short_name: [studyShortName] },
   });
 
-  if (loading) return <CircularProgress />;
-  if (error || !data) {
-    return (
-      <Typography variant="headline" color="error" size="sm">
-        {error ? `An error has occurred in loading stats component: ${error}` : 'Recieved wrong data'}
-      </Typography>
-    );
+  // Loading state
+  if (loading) {
+    return <CircularProgress />;
   }
-  return <StudyView data={data} studyCode="CMB" />;
+
+  // Error or no data state
+  if (error || !hasValidStudyData(data)) {
+    return <ErrorMessage error={error} />;
+  }
+
+  // Success state - render study view
+  return <StudyView data={data} study_short_name={studyShortName} study_id={studyId} />;
 };
 
 export default StudyDetailController;
