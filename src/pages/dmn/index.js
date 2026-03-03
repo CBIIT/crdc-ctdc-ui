183c38bcce3f5e7de547c5c999a9bb4ca681cd58
nathanhuangnih <nathaniel.huang@nih.gov>
2026-03-03 07:55:37 -0500

    added dropshadow below navbar

--- src/pages/dmn/index.js
+++ src/pages/dmn/index.js
@@ -4,12 +4,16 @@ import env from '../../utils/env';
 const DataModelNavigator = () => {
   const dmnUrl = env.REACT_APP_DMN_URL || 'https://cbiit.github.io/crdc-data-model-navigator/?config=https://raw.githubusercontent.com/CBIIT/ctdc-data-model-navigator-landing/refs/heads/main/example/CTDC/1.0.0/';
   return (
-    <div style={{ width: '100%', height: '1000px' }}>
+    <div style={{ 
+      width: '100%', 
+      height: '1000px',
+      boxShadow: 'inset 0 8px 10px -5px rgba(0, 0, 0, 0.3)',
+    }}>
       <iframe
         src={dmnUrl}
         scrolling="no"
         title="Data Model Navigator"
-        style={{ width: '100%', height: '1000px', border: 'none' }}
+        style={{ width: '100%', height: '100%', border: 'none' }}
         sandbox="allow-downloads allow-popups allow-scripts allow-same-origin"
       />
     </div>
